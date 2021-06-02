8e200348e88a579486f959cc9210aa7b94658249
helen <hhma@alumni.cpp.edu>
2021-06-02 12:58:46 -0400

    User MockBean instead of Mock

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -1,37 +1,57 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
+import guru.springframework.services.RecipeServiceImpl;
 import org.junit.Before;
-import org.junit.Test;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Created by jt on 6/19/17.
  */
+ @WebMvcTest
+@AutoConfigureMockMvc
+
 public class RecipeControllerTest {
 
 
-    @Mock
+    @MockBean
     RecipeService recipeService;
-
+    @Autowired
     RecipeController controller;
 
-    @Before
+    @Autowired
+    private MockMvc mockMvc;
+  /*  @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
         controller = new RecipeController(recipeService);
     }
-
+ */
     @Test
     public void testGetRecipe() throws Exception {
 
@@ -44,7 +64,22 @@ public class RecipeControllerTest {
 
         mockMvc.perform(get("/recipe/show/1"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("recipe/show"))
+                 .andExpect(view().name("recipe/show"))
                 .andExpect(model().attributeExists("recipe"));
+    }
+
+    @Test
+    public void testSaveOrUpdate() throws Exception {
+        RecipeCommand recipeCmdMock = new RecipeCommand();
+        recipeCmdMock.setId(1L);
+        recipeCmdMock.setDescription("Recipe Mock");
+
+        when(recipeService.saveRecipeCommand(ArgumentMatchers.any())).thenReturn(recipeCmdMock);
+        mockMvc.perform(post("/recipe")
+                .flashAttr("id", recipeCmdMock.getId())
+                .flashAttr("description", recipeCmdMock.getDescription()))
+                .andExpect(status().is3xxRedirection())
+                .andDo(print());
+       verify(recipeService).saveRecipeCommand(ArgumentMatchers.any());
     }
 }
